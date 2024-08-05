688513b8d406d637a4afb31ea8613f7471efee07
ArtemEvgTitov <artem.titov.wg@gmail.com>
2024-08-05 13:40:55 +0300

    fix: fix index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketPairs = {};
+    bracketsConfig.forEach(pair => {
+        bracketPairs[pair[1]] = pair[0];
+    });
+
+    const openBrackets = new Set(bracketsConfig.map(pair => pair[0]));
+    const closeBrackets = new Set(bracketsConfig.map(pair => pair[1]));
+
+    const stack = [];
+
+    for (let char of str) {
+        if (openBrackets.has(char) && closeBrackets.has(char)) {
+            if (stack.length > 0 && stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else if (openBrackets.has(char)) {
+            stack.push(char);
+        } else if (closeBrackets.has(char)) {
+            if (stack.length === 0 || stack[stack.length - 1] !== bracketPairs[char]) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+
+    return stack.length === 0;
 }
